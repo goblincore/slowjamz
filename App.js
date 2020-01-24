f5b653543029619f2750aed1c10cb30f0010d202
goblincore <donny@Donnys-MacBook-Pro.local>
2020-01-24 13:21:46 -0500

    add expofont and assets from playlist example

--- App.js
+++ App.js
@@ -9,6 +9,11 @@
 import React from 'react';
 import {
   SafeAreaView,
+  Dimensions,
+  Image,
+  Slider,
+  Stylesheet,
+  TouchableHighlight,
   StyleSheet,
   ScrollView,
   View,
@@ -31,6 +36,45 @@ import {
   Permissions,
 } from 'react-native-unimodules';
 import { Audio, Video } from 'expo-av';
+import * as Font from "expo-font";
+
+class PlaylistItem {
+  constructor(name, uri, isVideo) {
+    this.name = name;
+    this.uri = uri;
+    this.isVideo = isVideo;
+  }
+}
+
+const PLAYLIST = [
+  new PlaylistItem(
+    "Comfort Fit - “Sorry”",
+    "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Comfort_Fit_-_03_-_Sorry.mp3",
+    false
+  ),
+  new PlaylistItem(
+    "Big Buck Bunny",
+    "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+    true
+  ),
+  new PlaylistItem(
+    "Mildred Bailey – “All Of Me”",
+    "https://ia800304.us.archive.org/34/items/PaulWhitemanwithMildredBailey/PaulWhitemanwithMildredBailey-AllofMe.mp3",
+    false
+  ),
+  new PlaylistItem(
+    "Popeye - I don't scare",
+    "https://ia800501.us.archive.org/11/items/popeye_i_dont_scare/popeye_i_dont_scare_512kb.mp4",
+    true
+  ),
+  new PlaylistItem(
+    "Podington Bear - “Rubber Robot”",
+    "https://s3.amazonaws.com/exp-us-standard/audio/playlist-example/Podington_Bear_-_Rubber_Robot.mp3",
+    false
+  )
+];
+
+
 const App: () => React$Node = () => {
   return (
     <NavigationNativeContainer>
